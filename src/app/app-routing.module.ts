3bb1e49087afa2eafb68861b4f352f8142aa141b
Anthony Diaz <anthony.diaz2@unmsm.edu.pe>
2019-12-18 20:38:36 -0500

    Funcionando el main.js

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,23 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
+//Components
+import { NavbarComponent } from './components/shared/navbar/navbar.component';
+import { BannerComponent } from './components/home/banner/banner.component';
 
-const routes: Routes = [];
+
+const routes: Routes = [
+  
+  {
+    path: 'home',
+    component: NavbarComponent,
+    children: [{
+      path: '',
+      component: BannerComponent
+    }]
+  },
+  { path: '**', pathMatch: 'full', redirectTo: 'home' }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
